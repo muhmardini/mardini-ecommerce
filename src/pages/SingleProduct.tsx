be855773a1fbdea03f00e23e423e0c3b22289b01
muhammad k mardini <mardinimuh@gmail.com>
2026-04-01 20:27:07 +0300

    fixing the design on small devices for single product page

--- src/pages/SingleProduct.tsx
+++ src/pages/SingleProduct.tsx
@@ -13,19 +13,19 @@ const SingleProduct = () => {
   const product: Product = data.find((i:Product) => i.id ===  productId);
   if (!product) return <div>Loading...</div>;
   return (
-    <main className="flex flex-col md:flex-row py-24 min-h-screen justify-between items-start md:items-center ">
+    <main className="flex flex-col md:flex-row py-24 min-h-screen justify-between items-start md:items-center w-full">
       <div className="flex flex-col md:flex-1 px-8">
-        <div className="md:flex-3  flex justify-center border-b ">
+        <div className="md:flex-3 flex justify-center border-b ">
           <img
             className="md:w-full w-[50%] max-w-120"
             src={product?.images[0]}
             alt={product?.title}
           />
         </div>
-        <div className="md:flex flex-1 hidden">
+        <div className="md:flex md:flex-1 hidden">
           <div className="flex-2 flex justify-center ml-4">
             <img
-              className="w-60 max-w-45"
+              className="w-20 max-w-45"
               src={product?.images[1]}
               alt={product?.title + "(2)"}
             />
@@ -37,24 +37,24 @@ const SingleProduct = () => {
               alt={product?.title + "(3)"}
             />
           </div>
-          <div className="flex-1 flex flex-col justify-center items-center border-l ">
+          <div className="flex-1 flex flex-col justify-center items-center border-l">
             <img
               className="flex-1 w-30 border-b"
               src={product?.images[0]}
               alt={product?.title}
             />
             <img
-              className="flex-1  w-30"
+              className="flex-1  w-30 hidden md:block"
               src={product?.images[1]}
               alt={product?.title}
             />
           </div>
         </div>
       </div>
-      <div className="w-0.5 bg-gray-200 h-[80%] hidden md:block mx-10"></div>
+      <div className="w-px bg-basic h-120 hidden md:block md:mx-10"></div>
       <div className="flex-1 self-start mt-12 flex flex-col gap-y-4 px-4">
         <h1 className="text-secondary">{product?.title}</h1>
-        <p className="text-wrap md:text-[32px] text-2xl mt-4">{product?.description}</p>
+        <p className="text-wrap md:text-[32px] text-xl mt-4">{product?.description}</p>
         <div>
           {product?.tags.map((e, idx) => (
             <>
@@ -64,7 +64,7 @@ const SingleProduct = () => {
             </>
           ))}
         </div>
-        <div className="flex gap-2 items-start flex-colv">
+        <div className="flex gap-2 items-start flex-col">
           <div className="flex gap-4 items-center ">
             <h2>Colors:</h2>
             <div className="flex gap-2">
@@ -73,7 +73,7 @@ const SingleProduct = () => {
               ))}
             </div>
           </div>
-          <div className="flex gap-4 items-center ">
+          <div className="flex gap-4 items-center">
             <h2>Sizes:</h2>
             <div className="flex gap-4">
               {sizes.map((e: number) => (
@@ -85,13 +85,13 @@ const SingleProduct = () => {
         <p className="md:text-4xl text-2xl mt-4">$ {product?.price}</p>
         <div className="flex items-center gap-6 justify-center">
           <button
-            className="btn btn-primary md:px-12 px-8 py-2 md:text-xl text-lg shadow-lg"
+            className="btn btn-primary md:px-12 px-8 py-2 md:text-xl text-sm shadow-lg"
             type="button"
           >
             Buy Now
           </button>
           <button
-            className="btn btn-secondary md:px-12 py-2 px-8 md:text-xl text-lg shadow-xl"
+            className="btn btn-secondary md:px-12 py-2 px-8 md:text-xl text-sm shadow-xl"
             type="button"
           >
             Add to Cart
